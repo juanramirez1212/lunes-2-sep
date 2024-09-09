0eb09edcb2a376b184b991406b5a6a61544c4bbe
juanramirez1212 <165798346+juanramirez1212@users.noreply.github.com>
2024-09-08 22:58:14 -0500

    Add files via upload

---
+++ lunes 2 sep/ciclos.js
@@ -0,0 +1,22 @@
+suma = 0
+lei = 1
+while (i <= 100)
+    suma += i
+    i += 1
+    print("La suma de los números del 1 al 100 es:", suma)
+/////////////////////
+i = 1
+while (i <= 50)
+    if i % 2 != 0:
+        print(i)
+    i += 1
+/////////////////////
+i = 10
+while (i >= 1)
+    print(i)
+    i -= 1
+/////////////////////
+    i = 1
+    while (2**i <= 1024)
+        print(f("2^{i} = {2**i}"))
+        i += 1   
---
+++ lunes 2 sep/ejercicio.js
@@ -0,0 +1,10 @@
+
+suma = 0
+let i= 1
+while (i <= 100){
+    suma += i
+    i += 1
+    document.write("<br>"+i)
+}
+    
+  
